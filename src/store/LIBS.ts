6500693e1f3466ead9a6905ab511f8386884517a
XC327JDell <caoxuan8872@gmail.com>
2025-01-23 17:57:35 -0800

    update LIBS socket server ip

--- src/store/LIBS.ts
+++ src/store/LIBS.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:1234',  {transports: ['websocket']})
+const socket = io('http://192.168.50.2:1234',  {transports: ['websocket']})
 
 socket.on("connect", () => {
     console.log('socket io connected to LIBS')
